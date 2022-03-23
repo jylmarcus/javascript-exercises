cb3301f022112e3cdaafed862881531a863a55e0
jylmarcus <56386396+jylmarcus@users.noreply.github.com>
2022-03-23 11:04:30 +0800

    Update prototype based on test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,14 @@
 const sumAll = function(firstNumber, secondNumber) {
+  if (typeof(firstNumber) != "number" || typeof(secondNumber) != "number" || firstNumber < 0 || secondNumber <0) {
+    return "ERROR";
+  }
+  
   let sum = 0;
+  
   if (secondNumber < firstNumber) {
     [firstNumber, secondNumber] = [secondNumber, firstNumber];
   }
+  
   for (i = firstNumber; i <= secondNumber; i++){
     sum += i;
   }

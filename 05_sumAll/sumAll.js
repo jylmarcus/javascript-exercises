71dde795804a08e238dc136f5f124cfdbc72addb
jylmarcus <56386396+jylmarcus@users.noreply.github.com>
2022-03-23 10:58:16 +0800

    Added prototype for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
-};
+const sumAll = function(firstNumber, secondNumber) {
+  let sum = 0;
+  if (secondNumber < firstNumber) {
+    [firstNumber, secondNumber] = [secondNumber, firstNumber];
+  }
+  for (i = firstNumber; i <= secondNumber; i++){
+    sum += i;
+  }
 
 // Do not edit below this line
 module.exports = sumAll;

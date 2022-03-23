6862b69a3e37f663822f221a38ab13d0918fc401
jylmarcus <56386396+jylmarcus@users.noreply.github.com>
2022-03-23 10:32:31 +0800

    Wrote prototype function for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+  for (i=0; i < toRemove.length; i++) {
+    for (j = array.length-1; j >= 0; j--){
+      if (array[j] === toRemove[i]){
+        array = array.splice(j,1);
+      }
+    }
+  }
 };
+
+//ask sheng siong to call annie from acs
 
 // Do not edit below this line
 module.exports = removeFromArray;
